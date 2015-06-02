f38686fe235c3eb3ebff2eb4adc318d297cf9661
Anand Shah <anand.shah110@gmail.com>
2015-06-02 10:42:06 +0530

    Registered customer-edge with eureka

--- shopping-cart-customer-edge/src/main/java/org/test/shop/edge/ShoppingCartEdgeApp.java
+++ shopping-cart-customer-edge/src/main/java/org/test/shop/edge/ShoppingCartEdgeApp.java
@@ -2,6 +2,7 @@ package org.test.shop.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingCartEdgeApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartEdgeApp.class);
